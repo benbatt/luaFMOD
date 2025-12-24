679669518d90877879a1d45426629385cc463af4
Ben Batt <benbatt@gmail.com>
2025-12-24 23:39:05 +1100

    Add many new Channel methods

--- src/channel.c
+++ src/channel.c
@@ -21,16 +21,134 @@ DEALINGS IN THE SOFTWARE.
 #include "common.h"
 
 #define SELF_TYPE FMOD_CHANNEL
+#define FMOD_PREFIX FMOD_Channel_
 
-int METHOD_NAME(setPaused)(lua_State *L)
-{
-    GET_SELF;
+#include "templates.h"
 
-    int paused = lua_toboolean(L, 2);
-
-    RETURN_STATUS(FMOD_Channel_SetPaused(self, paused));
-}
+GET_HANDLE(FMOD_SYSTEM, SystemObject)
+PROPERTY(FMOD_BOOL, Paused)
+PROPERTY(float, Volume)
+PROPERTY(FMOD_BOOL, VolumeRamp)
+GET(float, Audibility)
+PROPERTY(float, Pitch)
+PROPERTY(FMOD_BOOL, Mute)
+PROPERTY_FLOAT_INDEXED(ReverbProperties)
+PROPERTY(float, LowPassGain)
+PROPERTY_CONSTANT(FMOD_MODE, Mode)
+GET_CUSTOM(FMOD_BOOL, isPlaying, IsPlaying)
+SET(float, Pan)
+SET_MULTI(MixLevelsOutput, float, float, float, float, float, float, float, float)
+HANDLE_LIST(FMOD_DSP, DSP)
+PROPERTY_MULTI(3DAttributes, FMOD_VECTOR, FMOD_VECTOR)
+PROPERTY_MULTI(3DMinMaxDistance, float, float)
+PROPERTY_MULTI(3DConeSettings, float, float, float)
+PROPERTY(FMOD_VECTOR, 3DConeOrientation)
+PROPERTY_MULTI(3DOcclusion, float, float)
+PROPERTY(float, 3DSpread)
+PROPERTY(float, 3DLevel)
+PROPERTY(float, 3DDopplerLevel)
+PROPERTY_MULTI(3DDistanceFilter, FMOD_BOOL, float, float)
+PROPERTY(float, Frequency)
+PROPERTY(int, Priority)
+PROPERTY(int, LoopCount)
+GET_CUSTOM(FMOD_BOOL, isVirtual, IsVirtual)
+GET(int, Index)
 
 METHODS_TABLE_BEGIN
-    METHODS_TABLE_ENTRY(setPaused)
+  METHODS_TABLE_ENTRY(getSystemObject)
+#if 0
+  METHODS_TABLE_ENTRY(stop)
+#endif
+  METHODS_TABLE_ENTRY(setPaused)
+  METHODS_TABLE_ENTRY(getPaused)
+  METHODS_TABLE_ENTRY(setVolume)
+  METHODS_TABLE_ENTRY(getVolume)
+  METHODS_TABLE_ENTRY(setVolumeRamp)
+  METHODS_TABLE_ENTRY(getVolumeRamp)
+  METHODS_TABLE_ENTRY(getAudibility)
+  METHODS_TABLE_ENTRY(setPitch)
+  METHODS_TABLE_ENTRY(getPitch)
+  METHODS_TABLE_ENTRY(setMute)
+  METHODS_TABLE_ENTRY(getMute)
+  METHODS_TABLE_ENTRY(setReverbProperties)
+  METHODS_TABLE_ENTRY(getReverbProperties)
+  METHODS_TABLE_ENTRY(setLowPassGain)
+  METHODS_TABLE_ENTRY(getLowPassGain)
+  METHODS_TABLE_ENTRY(setMode)
+  METHODS_TABLE_ENTRY(getMode)
+#if 0
+  METHODS_TABLE_ENTRY(setCallback)
+#endif
+  METHODS_TABLE_ENTRY(isPlaying)
+  METHODS_TABLE_ENTRY(setPan)
+  METHODS_TABLE_ENTRY(setMixLevelsOutput)
+#if 0
+  METHODS_TABLE_ENTRY(setMixLevelsInput)
+  METHODS_TABLE_ENTRY(setMixMatrix)
+  METHODS_TABLE_ENTRY(getMixMatrix)
+  METHODS_TABLE_ENTRY(getDSPClock)
+  METHODS_TABLE_ENTRY(setDelay)
+  METHODS_TABLE_ENTRY(getDelay)
+  METHODS_TABLE_ENTRY(addFadePoint)
+  METHODS_TABLE_ENTRY(setFadePointRamp)
+  METHODS_TABLE_ENTRY(removeFadePoints)
+  METHODS_TABLE_ENTRY(getFadePoints)
+#endif
+  METHODS_TABLE_ENTRY(getDSP)
+#if 0
+  METHODS_TABLE_ENTRY(addDSP)
+  METHODS_TABLE_ENTRY(removeDSP)
+#endif
+  METHODS_TABLE_ENTRY(getNumDSPs)
+#if 0
+  METHODS_TABLE_ENTRY(setDSPIndex)
+  METHODS_TABLE_ENTRY(getDSPIndex)
+#endif
+  METHODS_TABLE_ENTRY(set3DAttributes)
+  METHODS_TABLE_ENTRY(get3DAttributes)
+  METHODS_TABLE_ENTRY(set3DMinMaxDistance)
+  METHODS_TABLE_ENTRY(get3DMinMaxDistance)
+  METHODS_TABLE_ENTRY(set3DConeSettings)
+  METHODS_TABLE_ENTRY(get3DConeSettings)
+  METHODS_TABLE_ENTRY(set3DConeOrientation)
+  METHODS_TABLE_ENTRY(get3DConeOrientation)
+#if 0
+  METHODS_TABLE_ENTRY(set3DCustomRolloff)
+  METHODS_TABLE_ENTRY(get3DCustomRolloff)
+#endif
+  METHODS_TABLE_ENTRY(set3DOcclusion)
+  METHODS_TABLE_ENTRY(get3DOcclusion)
+  METHODS_TABLE_ENTRY(set3DSpread)
+  METHODS_TABLE_ENTRY(get3DSpread)
+  METHODS_TABLE_ENTRY(set3DLevel)
+  METHODS_TABLE_ENTRY(get3DLevel)
+  METHODS_TABLE_ENTRY(set3DDopplerLevel)
+  METHODS_TABLE_ENTRY(get3DDopplerLevel)
+  METHODS_TABLE_ENTRY(set3DDistanceFilter)
+  METHODS_TABLE_ENTRY(get3DDistanceFilter)
+#if 0
+  METHODS_TABLE_ENTRY(setUserData)
+  METHODS_TABLE_ENTRY(getUserData)
+#endif
+  METHODS_TABLE_ENTRY(setFrequency)
+  METHODS_TABLE_ENTRY(getFrequency)
+  METHODS_TABLE_ENTRY(setPriority)
+  METHODS_TABLE_ENTRY(getPriority)
+#if 0
+  METHODS_TABLE_ENTRY(setPosition)
+  METHODS_TABLE_ENTRY(getPosition)
+  METHODS_TABLE_ENTRY(setChannelGroup)
+  METHODS_TABLE_ENTRY(getChannelGroup)
+#endif
+  METHODS_TABLE_ENTRY(setLoopCount)
+  METHODS_TABLE_ENTRY(getLoopCount)
+#if 0
+  METHODS_TABLE_ENTRY(setLoopPoints)
+  METHODS_TABLE_ENTRY(getLoopPoints)
+#endif
+  METHODS_TABLE_ENTRY(isVirtual)
+#if 0
+  METHODS_TABLE_ENTRY(getCurrentSound)
+#endif
+  METHODS_TABLE_ENTRY(getIndex)
 METHODS_TABLE_END
